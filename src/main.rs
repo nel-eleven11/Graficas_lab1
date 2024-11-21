cd2db027ff27e12b78622746cb8ea93abea996ad
Nelson García Bravatti <gar22434@uvg.edu.gt>
2024-11-20 23:32:57 -0600

    Feat: Polygon 1 done

--- src/main.rs
+++ src/main.rs
@@ -8,30 +8,40 @@
  use line_impl::Line;
  use bmp::WriteBmp;
  use nalgebra_glm::vec3;
- 
+
  fn main() {
     let mut framebuffer = Framebuffer::new(800, 600);
 
-    // Define un polígono con múltiples vértices
-    let vertices = vec![
-        vec3(100.0, 150.0, 0.0),
-        vec3(300.0, 50.0, 0.0),
-        vec3(500.0, 150.0, 0.0),
-        vec3(450.0, 400.0, 0.0),
-        vec3(150.0, 400.0, 0.0),
-    ];
+    // Dibujar el Polígono 1
+    draw_polygon1(&mut framebuffer);
 
-    // Colores para el borde y el relleno
-    let border_color = 0x0000FF; // Azul
-    let fill_color = 0x00FF00;   // Verde
-
-    // Dibuja y rellena el polígono
-    framebuffer.draw_polygon(&vertices, border_color, fill_color);
-
-    // Guarda la imagen como BMP
+    // Guardar la imagen como BMP
     let output_file = "out.bmp";
     match framebuffer.render_buffer(output_file) {
         Ok(_) => println!("Image saved as {}", output_file),
         Err(e) => eprintln!("Error saving image: {}", e),
     }
+}
+ 
+ fn draw_polygon1(framebuffer: &mut Framebuffer) {
+    // Definir los vértices del Polígono 1
+    let vertices = vec![
+        vec3(165.0, 380.0, 0.0),
+        vec3(185.0, 360.0, 0.0),
+        vec3(180.0, 330.0, 0.0),
+        vec3(207.0, 345.0, 0.0),
+        vec3(233.0, 330.0, 0.0),
+        vec3(230.0, 360.0, 0.0),
+        vec3(250.0, 380.0, 0.0),
+        vec3(220.0, 385.0, 0.0),
+        vec3(205.0, 410.0, 0.0),
+        vec3(193.0, 383.0, 0.0),
+    ];
+
+    // Colores
+    let fill_color = 0x00FFFF;   // Amarillo (Hex: RGB)
+    let border_color = 0xFFFFFF; // Blanco (Hex: RGB)
+
+    // Dibujar el polígono
+    framebuffer.draw_polygon(&vertices, border_color, fill_color);
 }
